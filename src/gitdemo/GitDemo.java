2352848ede6900a3f0b91fb697009d01bd16b771
sindevje <sindevje@sheridancollege.ca>
2024-01-19 13:07:20 -0500

    NEW LINE

---
+++ src/gitdemo/GitDemo.java
@@ -0,0 +1,16 @@
+package gitdemo;
+/**
+ *
+ * @author devje
+ */
+public class GitDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("This is a git demo");
+        System.out.println("This is to be comitted");
+    }
+    
+}
